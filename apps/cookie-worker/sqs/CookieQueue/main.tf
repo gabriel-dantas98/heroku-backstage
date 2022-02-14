1f2dcac57d72fb74aa26bbee8c64110925c1f5eb
Gabriel Dantas Gomes <gabriel.dantas98@outlook.com.br>
2022-02-13 21:35:58 -0300

    Creating SQS CookieQueue by Backstage

---
+++ apps/cookie-worker/sqs/CookieQueue/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket  = "terraform-states"
+    region  = "us-east-1"
+    key     = "CookieQueue"
+  }
+}
+
+module "sqs" {
+
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-sqs"
+  version = "~> 2.0"
+
+  name = "CookieQueue"
+  
+  tags = {
+    Owner   = "team-d"
+    Service = "cookie-worker"
+  }
+
+}
